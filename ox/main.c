d2870d39cb265a7f01bd72f36908be07b89a59cc
Michael Henderson <quoha@Michaels-Mac-Pro.local>
2014-10-01 14:09:24 -0500

    Initial Commit

---
+++ ox/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ox
+//
+//  Created by Michael Henderson on 10/1/14.
+//  Copyright (c) 2014 Michael D Henderson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
