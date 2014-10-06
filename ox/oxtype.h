a24d3401b2c44ceed6997041ceccc0101e4b51d4
quoha <quoha@quoha.com>
2014-10-06 13:44:29 -0500

    document cell

--- ox/oxtype.h
+++ ox/oxtype.h
@@ -8,6 +8,103 @@
 #ifndef __ox__oxtype__
 #define __ox__oxtype__
 
+// atom   - type atom
+// list   - type list
+// symbol - type list, flagged as symbol
+//        - attributes stored in associative list
+//             (('name' 'pi') ('value' 3.14159))
+//
+// boolean
+//   ox does not have a boolean atom
+//   nill is always false
+//   everything else is always true
+//
+// lists and cons cells
+//
+//   ox does not support the traditional 'cons' operator
+//
+//   in lists, 'car' points to a data cell
+//             'cdr' points to  'nill' to end the list
+//                or points to the next cell in the list
+//       (car -> any valid data cell (including nill)
+//        cdr -> next cell in list or nill to terminate
+//       )
+//                cdr -> (car -> any valid data cell
+//                        cdr -> nill (terminates list)
+//                       )
+//               )
+//       )
+//
+// examples
+//   1
+//   atom: 1
+//
+//   ()
+//   atom: nill
+//
+//   ()
+//   car|cdr -> nill
+//    |
+//    v
+//   nill
+//
+//   (1 2 3)
+//   car|cdr -> car|cdr -> car|cdr -> nill
+//    |          |          |
+//    v          v          v
+//    1          2          3
+//
+//   (1 (2) 3)
+//   car|cdr -> car|cdr -> car|cdr -> nill
+//    |          |          |
+//    v          |          v
+//    1          |          3
+//               v
+//              car|cdr -> nill
+//               |
+//               v
+//               2
+//
+//   (key value)
+//   car|cdr -> car|cdr -> nill
+//    |          |
+//    v          v
+//   key       value
+//
+//   (1 (key value) (key value) 4)
+//   car|cdr -> car|cdr -> car|cdr -> car|cdr -> nill
+//    |          |          |          |
+//    v          |          |          v
+//    1          |          |          4
+//               |          v
+//               |         car|cdr -> car|cdr -> nill
+//               |          |          |
+//               |          v          v
+//               |         key       value
+//               |
+//              car|cdr -> car|cdr -> nill
+//               |          |
+//               v          v
+//              key       value
+//
+//   (nill)
+//   car|cdr -> car|cdr -> nill
+//    |          |
+//    v          v
+//   nill       nill
+//
+//   (nill (nill) nill)
+//   car|cdr -> car|cdr -> car|cdr -> nill
+//    |          |          |
+//    v          |          v
+//   nill        |          nill
+//               v
+//              car|cdr -> nill
+//               |
+//               v
+//              nill
+//
+
 enum atomType {eAtomFunc, eAtomInteger, eAtomReal, eAtomText, eAtomTime};
 
 typedef struct oxatom {
